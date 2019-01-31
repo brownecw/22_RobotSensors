2f08d0ba6f49d424b706aef95f0f4ddf60ed535e
brownecw <brownecw@rose-hulman.edu>
2019-01-31 09:36:41 -0500

    done with all of 24

--- src/m1_using_sensors.py
+++ src/m1_using_sensors.py
@@ -3,8 +3,8 @@ This module lets you practice the use of robot sensors.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Colin Browne, Parker Jordan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -25,7 +25,7 @@ import math
 def main():
     """ Calls the testing functions. """
     # Un-comment out these tests as you implement the methods they test.
-    # run_test_beep_and_tone()
+    run_test_beep_and_tone()
     # run_test_go_straight_for_seconds()
     # run_test_go_straight_for_inches_using_time()
     # run_test_go_straight_for_inches_using_sensor()
@@ -57,6 +57,17 @@ def run_test_beep_and_tone():
     #   in increments of 10, with 50 millisecond durations.
     #   Do not forget to apply the   wait   method to tone, as usual.
     # -------------------------------------------------------------------------
+    robo_beeper = Beeper()
+    robo_beeper.beep().wait()
+
+    robo_tone = ToneMaker()
+    robo_tone.tone(800,1000).wait()
+    robo_tone.tone(400,500).wait()
+    robo_tone.tone(600,500).wait()
+    for k in range(3):
+        robo_tone.tone(220 * (k + 1), 300).wait()
+
+
 
 
 # -----------------------------------------------------------------------------
@@ -355,7 +366,12 @@ class DriveSystem(object):
         self.go_straight_for_seconds(seconds, speed)
 
     def go_straight_for_inches_using_sensor(self, inches, speed):
-        pass
+        inches_per_degree = self.left_motor.WheelCircumference / 360
+        initial_position = self.left_motor.get_position()
+        while inches_per_degree * (self.left_motor.get_position() - initial_position) < inches:
+            self.go(speed, speed)
+        self.stop()
+
         # Live code this with students
 
     def go_straight_until_black(self, speed):
